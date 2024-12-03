70498ef7ef4b89b828d344a3046d34387020f324
AppleBoiy <work.chaipat@gmail.com>
2024-12-03 10:42:33 +0700

    ~ chore

--- python/edit_distance.py
+++ python/edit_distance.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-DEBUG = False
+DEBUG = True
 
 
 def edit_distance(s1, s2):
@@ -33,15 +33,45 @@ def edit_distance(s1, s2):
 
     return dp[m][n]
 
+def levenshtein_distance(s1, s2):
+    l1 = len(s1)
+    l2 = len(s2)
+
+    dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+    for i in range(l1 + 1):
+        dp[i][0] = i
+
+    for j in range(l2 + 1):
+        dp[0][j] = j
+
+    for i in range(1, l1 + 1):
+        for j in range(1, l2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                insert = dp[i][j - 1] + 1
+                delete = dp[i - 1][j] + 1
+                replace = dp[i - 1][j - 1] + 1
+                dp[i][j] = min(insert, delete, replace)
+
+    return dp[l1][l2]
 
 def test_edit_distance():
     s1 = "execution"
     s2 = "intention"
     assert edit_distance(s1, s2) == 8
 
+def test_levenshtein_distance():
+    s1 = "execution"
+    s2 = "intention"
+    assert levenshtein_distance(s1, s2) == 5
 
-if __name__ == "__main__":
+def main():
     w1 = input("Enter first word: ")
     w2 = input("Enter second word: ")
 
     print(f"Edit distance between {w1} and {w2} is {edit_distance(w1, w2)}")
+
+
+if __name__ == "__main__":
+    test_levenshtein_distance()
